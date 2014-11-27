45182c600ddd1f3eb4d731a4d2284805fb438a23
Vladimir Ignatev <ya.na.poche@gmail.com>
2014-11-27 16:44:53 +0400

    Removed Django legacy. Support Django 1.7

--- demo/settings.py
+++ demo/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'demo.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
