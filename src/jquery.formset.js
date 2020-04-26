c5e5fd472a2e35871c11e715eddda412aa4fb738
diophantus7 <bademeister7@web.de>
2020-04-26 14:02:49 +0200

    bug corrected

--- src/jquery.formset.js
+++ src/jquery.formset.js
@@ -92,6 +92,7 @@
                         del.val('on');
                         row.hide();
                         forms = $('.' + options.formCssClass).not(':hidden');
+                        totalForms.val(forms.length);
                     } else {
                         row.remove();
                         // Update the TOTAL_FORMS count:
