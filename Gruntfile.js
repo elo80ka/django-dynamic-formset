572f5044d6dcb3ec6817bea280199f659402f90e
Stanislaus Madueke <stan.madueke@gmail.com>
2023-08-08 04:08:49 +0100

    Update build dependencies (#199)
    Recreate package.json and update build dependencies and tests.
    
    * Update build dependencies
    * Remove unused libraries

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,29 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        qunit: {
-            'jquery.formset': ['tests/index.html']
+      pkg: grunt.file.readJSON("package.json"),
+      qunit: {
+        "jquery.formset": ["tests/**/*.html"],
+        options: {
+          puppeteer: {
+            headless: 'new',
+          },
         },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> (v<%= pkg.version %>) <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        }
+      },
+      uglify: {
+        options: {
+          banner:
+            '/*! <%= pkg.name %> (v<%= pkg.version %>) <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        },
+        build: {
+          src: "src/<%= pkg.name %>.js",
+          dest: "build/<%= pkg.name %>.min.js",
+        },
+      },
     });
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     
-    grunt.registerTask('default', ['uglify', 'qunit']);
+    grunt.registerTask("build", ["qunit", "uglify"]);
 };
